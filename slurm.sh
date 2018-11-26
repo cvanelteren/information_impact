a045cfbca179d237202d2e8e538593e683deb92c
cvanelteren <caspervanelteren@gmail.com>
2018-11-26 19:35:00 +0100

    added slurm file
    Former-commit-id: 4413c74101587ff7819b167ae8525e2265b5979b

---
+++ slurm.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -N 1      # nodes requested
+#SBATCH -n 1      # tasks requested
+#SBATCH -c 16      # cores requested
+#SBATCH -o outfile  # send stdout to outfile
+#SBATCH -e errfile  # send stderr to errfile
+#SBATCH -t 10000000:00:00  # time requested in hour:minute:second
+source activate
+cd /home/cveltere/it
+python setup.py build_ext --inplace
+srun python run.py
