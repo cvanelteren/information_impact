35345cbde05c3ef6714849b1e059207b138a02b5
cvanelteren <caspervanelteren@gmail.com>
2019-03-04 11:04:53 +0100

    removed deprecated functions from plotting

--- Utils/plotting.py
+++ Utils/plotting.py
@@ -62,68 +62,8 @@ def c():
     close('all')
 
 
-def showIDTCENT(idt, model, cent = 'betweenness'):
-    from functools import partial
-    # options
-    cents = dict(betweenness = partial(nx.betweenness_centrality, weight = 'weight'),\
-                 closeness = partial(nx.closeness_centrality, distance = 'weight'), \
-                 degree_weighted = partial(nx.degree, weight = 'weight'),\
-                 degree = nx.degree,\
-                 current_flow = nx.current_flow_betweenness_centrality,\
-                 current_flow_weighted = partial(nx.current_flow_betweenness_centrality, weight = 'weight'),\
-                 communicability = nx.communicability_betweenness_centrality,\
-                 eigenvector = nx.eigenvector_centrality,
-                 katz = nx.katz_centrality)
-    condList = 'closeness'
-    if cent in condList:
-        h = nx.get_edge_attributes(model.graph, 'weight')
-        h = {key: abs(value) for key, value in h.items()}
-        nx.set_edge_attributes(model.graph, h, 'weight')
-    colors = cm.get_cmap('tab20')(arange(0, model.nNodes))
-    fig, ax = subplots()
 
-    centData = cents[cent](model.graph)
-    print(centData)
-    for color, (name, idx) in zip(colors, model.mapping.items()):
-        ax.scatter(centData[name], idt[idx], color = color, label = name)
-    setp(ax,**dict(xlabel = cent, ylabel = 'idt'))
-    ax.legend()
-    return centData
-#%%
-#from information import fit  # move this in here?
-def showIDT(model, results, func = lambda x, a, b, c : exp(-(b * x)**c) ,
-            condition = 'abs'):
-    '''
-    Produces a boxplot per conditions, it labels the outliers with the label in the graph
-    : condition: determine which one to select:
-            - abs   = absolute idt
-            - rel   = relative idt [rate]
-            - auc   = area under the curve
-            - errors= fit error SSE
-    '''
-    selector = {name : idx for idx, name in enumerate(['abs', 'rel', 'auc', 'errors'])} # selector factors
-    I = array([result['I'].T for idx, result in results.items()]) # get mutual information
-    idts =  []  # TODO [change] :tmp list assignment as changes are being made to fit exponential
-    # for each condition fit the exponential
-    for i in I:
-        idts.append(fit(i, func)[0])
-    idts = array(idts)
-    fig, ax = subplots();
-    points = ax.boxplot(idts[..., selector[condition]].T, labels = list(results.keys()),
-                        )
-    # for every condition we need to show the outlier labels
-    for idx, outliers in enumerate(points['fliers']):        # for some reason called fliers
-        outlierValuesX, outlierValuesY = outliers.get_xdata(), outliers.get_ydata() # returns the possible outliers
-        for  x, y in zip(outlierValuesX, outlierValuesY): # map back to which index this is
-            errors     = (idts[idx, : , selector[condition]] - y)**2
-            outlieridx = argmin(errors) # least square index find
-            for name, idx in model.mapping.items():
-                if idx == outlieridx:
-                    print('>', idx, outlieridx)
-                    ax.text(x + .1, y, name)
-    ax.set_ylabel(condition)
-    # show()
-    return array(idts), points
+
 # %%
 def showImpact(model, results, showBoth = 'without'):
     # TODO: do we need to include the node being nudged or not? -> only works if only 1 is nudged
@@ -166,237 +106,11 @@ def showImpact(model, results, showBoth = 'without'):
     setp(ax, **formatting)
     return impacts
 
-# %%
-def showGraph(graph, attr = 'weight'):
-    '''
-    Shows the adjacency matrix and the 'graphical layout'
-    '''
-    # TODO: add curved arreas
-    # TODO: plot add zeros
-    # ugly af
-#    style.use('seaborn-white')
-    fig, axes = subplots(1, 2)
-    u = unique(list(nx.get_edge_attributes(graph, attr).values()))
-    cmap = cm.get_cmap('viridis')
-#    cmap = cm.get_cmap('viridis')(linspace(-1, 1, 20))
-#    cmap[0,:] = 0
-#    cmap.set_over('DarkViolet')
-    # adjacency plot
-    adj = nx.adjacency_matrix(graph, weight = 'weight').todense()
 
-#    adj = (adj - adj.min()) / (adj.max() - adj.min())
-#    adj[adj == 0] = nan
-    cmap.set_bad('gray', .2)
-
-    h = axes[0].imshow(\
-            adj, aspect = 'auto',\
-            cmap = cmap,\
-            )
-
-    cbar = colorbar(h, ax = axes[0], pad = .01)
-    text(1.15, .5 , 'weight', rotation = 90,\
-         horizontalalignment='center', \
-         fontsize           = 20,\
-         transform          = axes[0].transAxes)
-
-    cbar.set_ticks([round(min(u),2), round(max(u), 2)])
-    x = arange(len(graph.nodes()))
-    axes[0].set_xticks(x)
-    axes[0].set_xticklabels(graph.nodes(), ha='center', va = 'top', minor=False)
-    axes[0].set_yticks(x)
-    axes[0].set_yticklabels(graph.nodes(), ha = 'center', minor = False)
-    axes[0].tick_params(axis='y', which='major', pad= 35)
-#    cbar.set_ticklabels([])
-    setp(axes[0], **dict(xlabel = 'node', ylabel = 'node'))
-    fig.autofmt_xdate() # slant overlapping labels
-    # there probably is a wrapper keyword for this....
-
-    from matplotlib.patches import FancyArrowPatch, Circle, ConnectionStyle
-    # graphical layout
-
-    edges = nx.get_edge_attributes(graph, 'weight')
-    negatives = {}
-    positives = {}
-    maxWeight = max(list(edges.values()))
-    for key, value in edges.items():
-        value = round(value, 3)
-        edges[key] = value
-        if value < 0:
-            negatives[key] = value
-        else:
-            positives[key] = value
-
-    r = 2
-    pos = nx.circular_layout(graph, scale = 20)
-    for n in graph:
-        c=Circle(positions[n], radius=r,alpha=1, facecolor = 'white',\
-                 edgecolor = 'black', linewidth  = 1, linestyle = 'solid')
-        text(*c.center, n, horizontalalignment = 'center', verticalalignment = 'center')
-        axes[1].add_patch(c)
-        graph.node[n]['patch']=c
-        x,y=positions[n]
-#    pos = nx.circular_layout(graph, scale = .5)
-#    tmp = nx.get_edge_attributes(graph, 'weight')
-#    tmp = {key: abs(value) for key, value in tmp.items()}
-#    nx.set_edge_attributes(graph, tmp, 'absweight')
-#    pos = nx.kamada_kawai_layout(graph, weight=  'absweight', scale= 100000)
-#    patches = nx.draw_networkx_nodes(graph, pos = pos, alpha = 1, node_size = 1500, edgecolors = 'black', node_color = 'white')
-    seen={}
-    from copy import copy
-    for u, v in graph.edges():
-        n1= graph.node[u]['patch']
-        n2= graph.node[v]['patch']
-        d = graph[u][v]['weight']
-        rad=0.1
-        if (u,v) in seen:
-            rad=seen.get((u,v))
-            rad=(rad+np.sign(rad)*0.1)*-1
-            print(u,v )
-        if u == v:
-            j = .1
-            n2 = copy(n1)
-            n1 = copy(n1)
-            n1.center = (n1.center[0] + r, n1.center[1] )
-            n2.center = (n2.center[0] - r, n2.center[1])
-#            n1.center = tuple(i - j for i in n1.center)
-#            n2.center = tuple(i + j for i in n2.center)
-            print(n1, n2)
-            rad = 1.6
-            print(n1.center, n2.center)
-            e = FancyArrowPatch(n2.center, n1.center, connectionstyle = f'arc3,rad={rad}',\
-                                arrowstyle = '-|>',\
-                                mutation_scale = 10.0,
-                                lw = 2,
-                                alpha = abs(d),
-                                color = 'red' if d < 0 else 'green')
-            e.set_arrowstyle(arrowstyle= '-|>', head_length = 1.5)
-        else:
-            connectionstyle = f'arc3,rad={rad}'
-            e = FancyArrowPatch(n1.center,n2.center, patchA = n1, patchB = n2,\
-                                arrowstyle = '->',
-                                connectionstyle = connectionstyle,
-                                mutation_scale = 10.0,
-                                lw=2,
-                                alpha=  abs(d),
-                                color= 'red' if d < 0 else 'green')
-
-#        if (u,v) not in seen:
-#            center = (n1.center + n2.center) / 2 + random.rand()
-#            text(*center, round(d,2))
-        seen[(u,v)]=rad
-        axes[1].add_patch(e)
-#    nx.draw_networkx_labels(graph, pos = pos,\
-#                                          node_labels = graph.nodes())
-#
-#    nx.draw_networkx_edges(graph,
-#                           pos,
-#                           edgelist = negatives,
-#                           width =  list(negatives.values()), edge_color = 'red',
-#                           edge_labels = negatives, ax = axes[-1])
-#    nx.draw_networkx_edges(graph,
-#                           pos,
-#                           edgelist = positives,
-#                           width = list(positives.values()), edge_color =  'green',
-#                           edge_labels = positives, ax = axes[-1]
-#                           )
-#    nx.draw_networkx_edge_labels(graph, pos, edge_labels = edges, ax = axes[-1], label_pos  = .5)
-    axes[-1].autoscale()
-    axes[-1].axis('off')
-
-    # show()
-def showRank(model, I, func):
-
-    idts, coefs = fit(I.T, func)
-    maxidt = idts[:,0].max()
-    x = logspace(0, maxidt, 100)
-    rank = zeros((len(idts), len(x)))
-    for idx, xi in enumerate(x):
-        data = array([func(xi, *coef) for coef in coefs])
-        rank[:, idx] = argsort(data)
-    colormap = cm.get_cm('tab20')(arange(model.nNodes))
-#    fig, ax =
-
-def showDegree(graph):
-    deg = {key : value  for key, value in nx.degree(graph)}
-    x  = list(deg.values())
-    y  = [i / sum(x) for i in x]
-    fig, ax = subplots()
-    ax.plot(x, y, '.')
-    ax.set_xlabel('Degree (k)')
-    ax.set_ylabel('p(k)')
-
-def showIMT(model, I):
-    fig, ax = subplots()
-    ax.plot(I)
-    labels = zeros(model.nNodes, dtype = object)
-    for key, name in model.mapping.items():
-        labels[name] = key
-    ax.legend(labels)
-    setp(ax, **dict(xlabel = '$t - \delta$', ylabel = '$I(x_i^{t - \delta} ; X^T)$'))
-    ax.grid()
-    # show()
 
 def c():
     close('all')
 
-def showFit(model, I, \
-            func = lambda x, a, b, c: a + b*exp(-c * x)  , \
-            verbose = False):
-    style.use('seaborn-poster') # large texts(!)
-    fig, ax = subplots()
-    from matplotlib import patheffects as pe
-    colors = get_cmap(name = 'tab20')(arange(model.nNodes + 1))
-    idts, coefs = fit(I.T, func, verbose = verbose)
-    frmt = '^ s o _'.split(' ') # format for markers
-
-    # some plotting indices
-    n  = I.shape[-1]
-    x  = arange(0, n)      # for plotting the observed
-    num = 5000
-    xx = linspace(0, n, num) if n > max(idts[:,0]) else linspace(\
-                 0, max(idts[:,0]), num) # for plotting the fit
-#    print(I.shape, coefs.shape, colors.shape); assert 0
-    for idx, coef in enumerate(coefs):
-#        name = model.rmapping[idx]
-        i       = I[idx, :]
-
-        color   = colors[idx, :]
-        ax.scatter(x, i, c = color)                # observed points
-        ax.plot(xx, func(xx, *tuple(coef)), c = color)     # fit
-
-        # abs IDT
-        ax.plot(idts[idx, 0], func(idts[idx, 0], *tuple(coef)),\
-                marker = frmt[0], \
-                c = colors[idx, :],\
-                markersize = 15)
-        # rel IDT
-        ax.plot(idts[idx, 1],\
-                func(idts[idx, 1], *tuple(coef)),\
-                marker = frmt[1], c = color, \
-                markersize = 15)
-
-    # make label for every type not every plot
-    delta = .5 # hide labels
-    nodeLabels = [ax.plot(nan, '.', label = model.rmapping[i], c = colors[i, :])[0] for i, _ in enumerate(coefs)]
-    typeLabels = [ax.scatter(-10, -10, marker = type, \
-                          edgecolors = 'black', facecolor = 'black', label = l) \
-                    for type, l in zip(frmt, 'abs rel observed fit'.split(' '))\
-                  ]
-#    fitLabel  = ax.plot(-10, -10, 'k-', label = 'fit')
-
-    # make legends
-    nodeLegend = ax.legend(handles = nodeLabels, loc = 'upper right', title = 'Nodes')
-    typeLegend = ax.legend(handles = typeLabels, loc = 'upper center', title = 'Symbols')
-#    fitLegend  = ax.legend(handles = fitLabel,   loc = 'center right')
-    [gca().add_artist(i) for i in [nodeLegend, typeLegend]]
-
-
-    formatting = dict(xlabel = '$t - \delta$', ylabel = 'Mutual information', \
-                      title = 'updateType{}'.format(model.mode), \
-                      ylim = [-.01, I.max() +  delta],
-                      xlim = [-1, max(xx) + 4 * delta])
-    setp(ax, **formatting)
-    return idts, coefs
 #close('all');
 #showGraph(model.graph)
 def saveAllFigures(useLabels = False, path = '../Figures'):
@@ -523,13 +237,13 @@ def addGraphPretty(graph, ax, \
         if u == v:
             n2 = copy(n1)
             n1 = copy(n1)
-            theta  = random.uniform(0, 2*pi)
+            theta     = random.uniform(0, 2*pi)
             r         = circlekwargs.get('radius')
             rotation  = pi
             corner1   = array([sin(theta), cos(theta)]) * r
             corner2   = array([sin(theta + rotation), cos(theta + rotation)]) *\
             r + .12 * sign(random.randn()) * r
-
+            ax.annotate(*c.center)
             n1.center = array(n1.center) + corner1
             n2.center = array(n2.center) + corner2
 
--- test.py
+++ test.py
@@ -5,42 +5,9 @@ Created on Mon Feb 11 08:55:35 2019
 
 @author: casper
 """
-<<<<<<< HEAD
-#
-#import networkx as nx
-#import msgpack
-#from numpy import *
-#from matplotlib.pyplot import *
-#from Models.fastIsing import Ising
-#
-#g = nx.barabasi_albert_graph(20, 10)
-#temps = linspace(0, 100, 100)
-#
-#m = Ising(g)
-#
-#mags, sus = m.matchMagnetization(temps)
-#
-#fig, ax = subplots()
-#ax.plot(temps, mags, temps, sus)
-#fig.show()
-#show()
-
-
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.spatial import ConvexHull, Delaunay
-
-x = np.linspace(-np.pi, np.pi)
-p = np.random.rand(100, 2)
-p = np.array([np.cos(x), np.sin(x)]).T
-hull = ConvexHull(p)
-fig, ax = plt.subplots()
-ax.scatter(*p.T, color = 'red')
-ax.plot(*p[hull.simplices].T)
-fig.show()
-=======
 
 import networkx as nx
+nx.barabasi_albert_graph
 #import msgpack
 #from numpy import *
 from Models.fastIsing import Ising
@@ -65,9 +32,7 @@ m = Ising(g)
 #        # recompute data limits
 #        ax.relim()
 #        ax.axes.autoscale_view(True, True, True)
-#        
+#
 #        # update figure; flush events
 #        fig.canvas.draw()
 #        fig.canvas.flush_events()
-
->>>>>>> 65ca8ec635ca46bccf513a8b25177f2969076cf6
