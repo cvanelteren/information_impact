76402ff031ae3ee2502eb48316a1150d19e108bf
cvanelteren <caspervanelteren@gmail.com>
2019-04-18 19:20:01 +0200

    reperform tests

--- slurm.sh
+++ slurm.sh
@@ -4,7 +4,7 @@
 #SBATCH -c 16      # cores requested
 #SBATCH -o outfile  # send stdout to outfile
 #SBATCH -e errfile  # send stderr to errfile
-#SBATCH -t 10000:00:00  # time requested in hour:minute:second
+#SBATCH -t 48:00:00  # time requested in hour:minute:second
 source activate
 cd $HOME/information_impact
 python setup.py build_ext --inplace
