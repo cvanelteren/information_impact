31d1d9326e97a3911e596708c37315c1eda8510c
cvanelteren <caspervanelteren@gmail.com>
2019-04-18 23:55:30 +0200

    potts model nearly done
    Former-commit-id: 894173a498e35551bf21e298d5fda36f09dde84a

--- throw.py
+++ throw.py
@@ -1,7 +1,18 @@
 import matplotlib as mpl
-from Models import fastIsing
-import networkx as nx
+from Models import  fastIsing, potts
+import networkx as nx, numpy as np
 
-g = nx.grid_2d_graph(10, 10)
+n = 30
+g = nx.grid_2d_graph(n, n)
 
-m = fastIsing.Ising(g)
+#m = fastIsing.Ising(g)
+m = potts.Potts(g, temperature = .5)
+
+res = m.simulate(190)
+
+
+import matplotlib.pyplot as plt
+print(res)
+fig, ax = plt.subplots()
+ax.imshow(res[-1].reshape(n, n), aspect = 'auto')
+fig.show()
