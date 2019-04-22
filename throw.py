4796f5f21379a4c4fd9e60e66a42c902bbc3e2a9
cvanelteren <caspervanelteren@gmail.com>
2019-04-22 14:24:44 +0200

    added criminal data

--- throw.py
+++ throw.py
@@ -7,17 +7,38 @@ g = nx.grid_2d_graph(n, n)
 
 #m = fastIsing.Ising(g)
 
-m = potts.Potts(g, temperature = 0, updateType = 'sync', agentStates = [0, 1])
-temps = np.linspace(0, 3)
+m = potts.Potts(g, temperature = 0, updateType = 'async', agentStates = [0, 1])
+temps = np.linspace(0, 10, 30)
+
+n = 50
+a, b = m.matchMagnetization(temps, n)
+
 
 fig, ax = plt.subplots()
-for t in temps:
-    m.t = t
-    m.reset()
-    res = m.simulate(1000)
-    res = np.array([m.siteEnergy(i) for i in res])
-    ax.scatter(t, -res.mean())
+tax = ax.twinx()
+ax.plot(temps, a)
+tax.plot(temps, b, 'r')
 fig.show()
 
 
+fig, ax = plt.subplots()
+r = []
+for t in temps:
+    m.t = t
+    m.reset()
+    res = m.simulate(n)
+    res = np.array([m.siteEnergy(i) for i in res])
+    r.append(res.sum() / (n * 30 * 30))
+#    ax.scatter(t, res)
+ax.plot(temps, r)
+#m = fastIsing.Ising(g)
+#a, b = m.matchMagnetization(temps, n)
+#fig, ax = plt.subplots()
+#tax = ax.twinx()
+#ax.plot(temps, a)
+#tax.plot(temps, b, 'r')
+#fig.show()
 
+
+plt.show()
+# %%
