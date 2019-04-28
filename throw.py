b9da23495ccb6bd8f74065a3067209d0b8985409
cvanelteren <caspervanelteren@gmail.com>
2019-04-28 13:47:25 +0200

    cleaned up the code

--- throw.py
+++ throw.py
@@ -6,30 +6,33 @@ import networkx as nx, numpy as np
 n = 10
 g = nx.grid_2d_graph(n, n)
 
-#m = fastIsing.Ising(g)
+m = fastIsing.Ising(graph = g)
 
-m = potts.Potts(g, temperature = 0, \
+
+#assert 0
+
+m = potts.Potts(graph = g, temperature = 0, \
                 updateType = 'async', \
                 agentStates = [0, 1, 2], \
                 memorySize = 3)
 
-# print(m.memorySize, m.memory.base)
+## print(m.memorySize, m.memory.base)
 temps = np.linspace(0, 10, 30)
-temps = np.logspace(-10, 1, 30)
-
+#temps = np.logspace(-10, 1, 30)
+##
 N = 1000
-
-#print(m.memory.base.shape)
+#
+##print(m.memory.base.shape)
 a, b = m.matchMagnetization(temps, N)
-
-print(m.memory.base)
-#print(m.memory.base)
-dtype = int
-fig, ax = plt.subplots()
-tax = ax.twinx()
-ax.plot(temps, a )
-tax.plot(temps, b, 'r')
-fig.show()
+#
+##print(m.memory.base)
+##print(m.memory.base)
+#dtype = int
+#fig, ax = plt.subplots()
+#tax = ax.twinx()
+#ax.plot(temps, a )
+#tax.plot(temps, b, 'r')
+#fig.show()
 
 
 #fig, ax = plt.subplots()
@@ -43,17 +46,13 @@ fig.show()
 ##    ax.scatter(t, res)
 #ax.plot(temps, r)
 ##m = fastIsing.Ising(g)
-#a, b = m.matchMagnetization(temps, n)
-#fig, ax = plt.subplots()
-#tax = ax.twinx()
-#ax.plot(temps, a)
-#tax.plot(temps, b, 'r')
-#fig.show()
+fig, ax = plt.subplots()
+tax = ax.twinx()
+ax.plot(temps, a)
+tax.plot(temps, b, 'r')
+fig.show()
 
 
 plt.show()
 # %%
 
-import time
-test = dict(repeats = 10, deltas = 12)
-print(f'test/{time.time()}' + ''.join(f"_{k}={v}" for k, v in test.items()))
