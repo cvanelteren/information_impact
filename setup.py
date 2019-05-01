911768519e139afc3a31f40179b90c8fb3d9fa6c
cvanelteren <caspervanelteren@gmail.com>
2019-05-01 10:17:19 +0200

    simpler setup script

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
 # from distutils.core import setup, Extension
 from setuptools import setup
 from setuptools.extension import Extension
-try:
-    from Cython.Build import cythonize
-    from Cython.Distutils import build_ext
-    import numpy, os, multiprocessing as mp
-except:
-    raise ModuleNotFoundError("Requirement(s) not present, please see setup.py")
 
 
 # MODULE INFORMATION
@@ -16,16 +10,28 @@ __email__               = "caspervanelteren@gmail.com"
 __description__         = "General toolbox for modelling discrete systems using information theory"
 __license__             = "MIT"
 __python__requires__    = ">=3.6"
+__version__             = '1.0'
 
 
 # clang seems faster on my machine
-# check whether it is running on the cluster
-if not 'fs4' in os.uname().nodename:
+from subprocess import run
+import re, os
+clangCheck = run("clang --version".split(), capture_output= True)
+if not clangCheck.returncode:
+    version =  re.findall('(\d+.)+', str(clangCheck.stdout))[0]
+    if version <= '8.0.0':
+        print("Warning older clang version used. May not compile")
     os.environ['CXXFLAGS'] = "clang++ -Xclang -fopenmp -Wall -fno-wrapv -fast-math -Ofast -std=c++17 -march=native"
     os.environ['CC']       = "clang++ -Xclang -fopenmp -Wall -fno-wrapv -ffast-math -Ofast -std=c++17 -march=native"
 
-exts = []
 
+run('conda install -c conda-forge --file requirements.txt'.split())
+
+from Cython.Build import cythonize
+from Cython.Distutils import build_ext
+import numpy, multiprocessing as mp
+# collect pyx files
+exts = []
 baseDir =  os.getcwd() + os.path.sep
 nums = numpy.get_include()
 if not os.path.exists('Data'):
@@ -51,35 +57,34 @@ for (root, dirs, files) in os.walk(baseDir):
                                                 ],\
                            extra_link_args = ['-fopenmp',\
                                               '-std=c++17',
-
                                               # '-g'\
                                               ],\
             )
             exts.append(ex)
 
-with open('requirements.txt', 'r') as f:
-    requirements =  f.readlines()
-requirements = [i.strip() for i in requirements]
-# compile
-with open('requirements.txt', 'r') as install_reqs:
-    setup(\
-        name            = __name__,\
-        author          = __author__,\
-        author_email    = __email__,\
-        python_requires = __python__requires__,\
-        zip_safe        = False,\
-        install_requires= requirements,\
-        ext_modules = cythonize(\
-                exts,\
-                # annotate            = True,\ # set to true for performance html
-                language_level      = 3,\
-                compiler_directives = dict(\
-                                        fast_gil       = True,\
-                                        # binding      = True,\
-                                        # embedsignature = True,\
-                                        ),\
-                # source must be pickable
-                nthreads            = mp.cpu_count(),\
-                ),\
-    # gdb_debug =True,
-    )
+
+# # compile
+# with open('requirements.txt', 'r') as f:
+#     install_dependencies = [i.strip() for i in f.readlines()]
+#
+setup(\
+    name            = __name__,\
+    author          = __author__,\
+    version         = __version__,\
+    author_email    = __email__,\
+    python_requires = __python__requires__,\
+    zip_safe        = False,\
+    ext_modules = cythonize(\
+            exts,\
+            # annotate            = True,\ # set to true for performance html
+            language_level      = 3,\
+            compiler_directives = dict(\
+                                    fast_gil       = True,\
+                                    # binding      = True,\
+                                    # embedsignature = True,\
+                                    ),\
+            # source must be pickable
+            nthreads            = mp.cpu_count(),\
+            ),\
+# gdb_debug =True,
+)
