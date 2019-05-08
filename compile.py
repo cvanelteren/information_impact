c7211570530832befe6cd1cec056bdfab0e2b82b
cvanelteren <caspervanelteren@gmail.com>
2019-05-08 14:58:05 +0200

    update slurm

--- compile.py
+++ compile.py
@@ -7,16 +7,17 @@ import numpy, multiprocessing as mp, os
 import re, os
 from subprocess import run
 clangCheck = run("clang --version".split(), capture_output= True)
+add = []
 if not clangCheck.returncode and 'fs4' not in os.uname().nodename:
     os.environ['CXXFLAGS'] = "clang++ -Xclang -fopenmp -Wall -fno-wrapv -fast-math -Ofast -std=c++11 -march=native"
     os.environ['CC']       = "clang++ -Xclang -fopenmp -Wall -fno-wrapv -ffast-math -Ofast -std=c++11 -march=native"
+    add.append('-lomp') # clang openmp stuff
 # collect pyx files
 exts = []
 baseDir =  os.getcwd() + os.path.sep
 nums = numpy.get_include()
 if not os.path.exists('Data'):
     os.mkdir('Data')
-
 for (root, dirs, files) in os.walk(baseDir):
     for file in files:
         fileName = f'{root}/{file}'
@@ -37,13 +38,10 @@ for (root, dirs, files) in os.walk(baseDir):
                                                 ],\
                            extra_link_args = ['-fopenmp',\
                                               '-std=c++11',\
-                                              '-lomp',\
                                               # '-g'\
-                                              ],\
+                                              ] + add,\
             )
             exts.append(ex)
-
-
 # # compile
 # with open('requirements.txt', 'r') as f:
 #     install_dependencies = [i.strip() for i in f.readlines()]
