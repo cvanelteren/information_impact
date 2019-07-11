51c12967671d840c2aef53bf51f7b3ddfe56b288
cvanelteren <caspervanelteren@gmail.com>
2019-07-11 14:25:00 +0200

    added CCA

--- run.py
+++ run.py
@@ -29,10 +29,10 @@ import networkx as nx, \
         time
 close('all')
 if __name__ == '__main__':
-    repeats       = int(1e4)
+    repeats       = int(1e5)
     deltas        = 30
     step          = int(1e3)
-    nSamples      = int(5e4)
+    nSamples      = int(1e5)
     burninSamples = 0
     pulseSizes    = [0.5, np.inf] #, -np.inf]# , .8, .7]
 
@@ -45,12 +45,12 @@ if __name__ == '__main__':
     graphs = []
     N  = 10
 
-#    for i in range(10):
-#        g = nx.barabasi_albert_graph(N, 2)
-#        g = nx.erdos_renyi_graph(N, .2)
-#        r = np.random.rand() * (1 - .2) + .2
-#        g = nx.duplication_divergence_graph(N, r)
-#        graphs.append(g)
+    for i in range(10):
+        r = np.random.rand() # * (1 - .2) + .2
+        # g = nx.barabasi_albert_graph(N, 2)
+        g = nx.erdos_renyi_graph(N, r)
+        # g = nx.duplication_divergence_graph(N, r)
+        graphs.append(g)
        # w = nx.utils.powerlaw_sequence(N, 2)
        # g = nx.expected_degree_graph(w)
         # g = sorted(nx.connected_component_subgraphs(g), key = lambda x: len(x))[-1]
@@ -62,15 +62,15 @@ if __name__ == '__main__':
 #    graphs[0].add_edge(0,0)
 #    for j in np.int32(np.logspace(0, np.log10(N-1),  5)):
 #       graphs.append(nx.barabasi_albert_graph(N, j))
-    dataDir = 'Graphs' # relative path careful
-    df    = IO.readCSV(f'{dataDir}/Graph_min1_1.csv', header = 0, index_col = 0)
-    h     = IO.readCSV(f'{dataDir}/External_min1_1.csv', header = 0, index_col = 0)
-    graph   = nx.from_pandas_adjacency(df)
-    attr = {}
-    for node, row in h.iterrows():
-        attr[node] = dict(H = row['externalField'], nudges = 0)
-    nx.set_node_attributes(graph, attr)
-    graphs.append(graph)
+    #dataDir = 'Graphs' # relative path careful
+    #df    = IO.readCSV(f'{dataDir}/Graph_min1_1.csv', header = 0, index_col = 0)
+    #h     = IO.readCSV(f'{dataDir}/External_min1_1.csv', header = 0, index_col = 0)
+    #graph   = nx.from_pandas_adjacency(df)
+    #attr = {}
+    #for node, row in h.iterrows():
+    #    attr[node] = dict(H = row['externalField'], nudges = 0)
+    #nx.set_node_attributes(graph, attr)
+    #graphs.append(graph)
     
     if 'fs4' in os.uname().nodename or 'node' in os.uname().nodename:
         now = datetime.datetime.now().isoformat()
