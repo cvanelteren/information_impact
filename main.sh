81788eaadbb20630f226bcc4c8958fdfbb46f514
cvanelteren <caspervanelteren@gmail.com>
2019-09-12 15:13:50 +0200

    massively parallal update

---
+++ main.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -N 1      # nodes requested
+#SBATCH -n 1      # tasks requested
+#SBATCH -c 16      # cores requested
+#SBATCH -o outfile  # send stdout to outfile
+#SBATCH -e errfile  # send stderr to errfile
+#SBATCH -t 48:00:00  # time requested in hour:minute:second
+source activate base
+cd $HOME/information_impact
+python compile.py build_ext --inplace
+srun python3 main.py 
