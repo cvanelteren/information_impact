8931ffed2399e6a6d94276a5cab176b51a4e094c
cvanelteren <caspervanelteren@gmail.com>
2019-09-13 15:53:25 +0200

    push

--- mainr.py
+++ mainr.py
@@ -1,5 +1,5 @@
 import os, datetime
-with open('simulations.txt') as f:
+with open('simulations.txt', 'r') as f:
     files = list(f.readlines())
 
 nodename = os.uname().nodename
@@ -10,12 +10,19 @@ if any([nodename in i for i in \
 else:
     runCommand = 'python3 single_run.py --file'
 
-from subprocess import Popen
+import subprocess
 if files:
+    a = len(files)
     runFile = files[0].strip('\n')
     files.pop(0)
-    Popen([*runCommand.split(), runFile])
+
+    # Popen([*runCommand.split(), runFile],\
+            #stdin = None, stdout = None,\
+            # stderr = None)
     with open('simulations.txt', 'w') as f:
         f.writelines([i + '\n' for i in files])
     # call itself
-    Popen('python mainr.py'.split(), stdin=None, stdout=None, stderr=None, close_fds=True)
+    print('calling myself')
+    subprocess.Popen('python mainr.py'.split())
+    print('new')
+    assert len(files) < a
