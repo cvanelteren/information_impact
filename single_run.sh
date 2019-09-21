559ee21b7d40a654d83bcdbab8c26e3ba13610de
cvanelteren <caspervanelteren@gmail.com>
2019-09-21 15:45:00 +0200

    update

--- single_run.sh
+++ single_run.sh
@@ -2,7 +2,7 @@
 #SBATCH -N 1      # nodes requested
 #SBATCH -n 1      # tasks requested
 #SBATCH -c 16      # cores requested
-#SBATCH -o outfile  # send stdout to outfile
+#SBATCH -o outfile$1  # send stdout to outfile
 #SBATCH -e errfile  # send stderr to errfile
 #SBATCH -t 48:00:00  # time requested in hour:minute:second
 #SBATCH --constraint=cpunode
