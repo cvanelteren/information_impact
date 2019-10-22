1b65280d12674337530bf43c17f19852b746faf4
cvanelteren <caspervanelteren@gmail.com>
2019-10-22 16:15:00 +0200

    added new files

--- removeOld.py
+++ removeOld.py
@@ -1,6 +1,7 @@
 import os, datetime
 
 root = '/var/scratch/cveltere/tester'
+root = 'Data/test/'
 THRESHOLD = datetime.datetime(\
 year = 2019,\
 day  = 18,\
@@ -11,12 +12,21 @@ hour = 17,\
 print(THRESHOLD)
 if __name__ == "__main__":
     delete = []
+    counter = 0
     for (root, dirs, fileNames) in os.walk(root):
         for file in fileNames:
+            counter += 1
             path = os.path.join(root, file)
-            ctime = os.path.getctime(path)
-            ctime = datetime.strptime(ctime,\
-            '%Y-%m-%d %H:%M:%S')
-            if ctime > THRESHOLD:
+            ctime = os.path.getmtime(path)
+            ctime = datetime.datetime.fromtimestamp(ctime)
+            # if file.endswith('.bk'):
+                # os.rename(path, path.split('.bk')[0])
+            print(ctime, THRESHOLD, file)
+            if ctime > THRESHOLD and 'settings' not in path:
                 delete.append(path)
-    print(len(delete), delete[:5])
+    for d in delete:
+        print(d)
+        assert 'settings' not in d
+        assert not d.endswith('.bk')
+        os.rename(d, d + '.bk')
+    print(len(delete), counter)
