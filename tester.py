2390021368e5cd3a3e1c80581a6c56b40892e402
cvanelteren <caspervanelteren@gmail.com>
2019-10-31 14:53:33 +0100

    cleaned git
    Former-commit-id: 7aa19b0b89ef5a64f40abec80ee9d907d38cd2bc

--- tester.py
+++
@@ -1,4 +0,0 @@
-from Models.test import test
-import numpy as np
-a = np.random.rand(10)
-print(test(a, len(a)).base)
--- tmp.sh
+++
@@ -1,7 +0,0 @@
-echo "number of simulations running:" 
-squeue | grep cveltere | wc -l
-echo "Number of simulations to do " 
-cat simulations.txt | wc -l 
-
-squeue | grep cveltere
-
