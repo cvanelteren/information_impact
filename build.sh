2bdc73d98375e8a2624d08e95df2f9f388b0248f
Casper van Elteren <caspervanelteren@gmail.com>
2020-04-02 10:57:56 +0200

    i hate bash
    Former-commit-id: 3582299ad0f3027ae3d6b47fa0520539be5f67b0

--- build.sh
+++ build.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-python compile.py build_ext --inplace 
+
+python compile.py build_ext --inplace
+while [ "$1" != "" ]; do
+    case $1 in
+        -t | --test)
+           python -m unittest tests.py
+           ;;
+        -h | --help)
+            exit 1
+            ;;
+        * )
+            usage
+            exit 1
+    esac
+    shift
+done
