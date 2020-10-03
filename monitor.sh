31f666adc8717811ba7ef98a5415a3cb9d0f47e9
Casper van Elteren <caspervanelteren@gmail.com>
2020-10-03 12:19:48 +0200

    new monitor addition
    Former-commit-id: 7c98d41b60207857131c18ebd9fb1c6f357afad8

--- monitor.sh
+++ monitor.sh
@@ -2,3 +2,4 @@ echo "Number of simulations running"
 squeue | grep cveltere | wc -l
 echo "Disk space in scratch" && du -sh /var/scratch/cveltere
 squeue | grep cveltere
+squeue | grep -u cveltere | akw "$0=$1" | cat *"$(xargs)""
