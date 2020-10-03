0369aba76383fdd9e76ca0a8c44bb4e773737969
Casper van Elteren <caspervanelteren@gmail.com>
2020-10-03 12:20:50 +0200

    new monitro
    Former-commit-id: d09d97d01f23c67af0bb8e2d25c0d60132f1fb4a

---
+++ slurm/monitor.sh
@@ -0,0 +1,5 @@
+echo "Number of simulations running"
+squeue | grep cveltere | wc -l
+echo "Disk space in scratch" && du -sh /var/scratch/cveltere
+squeue | grep cveltere
+cat *"$(squeue -u cveltere -h | awk '$0=$1'"*
