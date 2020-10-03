454725d04d1491f13612c2c006a68e6d5d2f6605
Casper van Elteren <caspervanelteren@gmail.com>
2020-10-03 11:52:42 +0200

    corrected file path
    Former-commit-id: d257deb3fbbba9346b5abba1c16d861f57d8dca7

--- slurm/slurm_toml.sh
+++ slurm/slurm_toml.sh
@@ -7,7 +7,7 @@
 #SBATCH -t 48:00:00  # time requested in hour:minute:second
 #SBATCH --constraint=cpunode
 source activate
-cd /var/scratch/information_impact
+cd /var/scratch/cveltere/information_impact
 python setup.py build_ext --inplace
 cd slurm
 srun python run_toml.py
