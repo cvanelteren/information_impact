b9ed7726b012177d1e5ce484ade46b232c868210
cvanelteren <caspervanelteren@gmail.com>
2020-10-21 23:57:17 +0200

    redo setup file

--- setup.py
+++ setup.py
@@ -15,25 +15,12 @@ cppv    = '17'
 flags = f'{optFlag} -march=native -std=c++{cppv} -flto '\
         '-frename-registers -funroll-loops -fno-wrapv '\
         '-fopenmp-simd -fopenmp -Wfatal-errors'
-try:
-    clangCheck = run(f"{compiler} --version".split(), capture_output= True)
-    if not clangCheck.returncode and 'fs4' not in os.uname().nodename:
-        print("Using default")
-        os.environ['CXXFLAGS'] =  f'{compiler} {flags}'
-        # os.environ['CC']       =  f'{compiler} {flags}'
-        # add.append('-lomp') # c
-except Exception as e:
-    print(e)
-    pass
 # collect pyx files
 exts = []
 # baseDir =  os.getcwd() + os.path.sep +
 
 BASE = "imi"
 nums = numpy.get_include()
-import plexsim
-pls = plexsim.__path__[0]
-print(pls)
 for (root, dirs, files) in os.walk(BASE):
     for file in files:
         fileName = os.path.join(root, file)
@@ -47,26 +34,27 @@ for (root, dirs, files) in os.walk(BASE):
 
             print(extPath, extName, extension)
             sources  = [extPath]
-            ex = Extension(extName, \
-                           sources            = sources, \
-                           include_dirs       = [nums, '.'],\
-                           extra_compile_args = flags.split(),\
-                           extra_link_args = ['-fopenmp',\
-                                              f'-std=c++{cppv}',\
-                                              # '-g'\
-                                              ] + add,\
+            ex = Extension(extName,
+                           sources            = sources,
+                           include_dirs       = [nums, '.'],
+                           extra_compile_args = flags.split(),
+                           language = "c++",
+                           extra_link_args = ['-fopenmp',
+                                              f'-std=c++{cppv}',
+                                              # '-g'
+                                              ] + add,
                             define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
             )
             exts.append(ex)
-cdirectives =  dict(\
-                    fast_gil         = True,\
-                    boundscheck      = False,\
-                    cdivision        = True,\
-                    initializedcheck = False,\
-                    overflowcheck    = False,\
-                    nonecheck        = False,\
-                    binding          = True,\
-                    # embedsignature = True,\
+
+cdirectives =  dict(
+                    boundscheck      = False,
+                    cdivision        = True,
+                    initializedcheck = False,
+                    overflowcheck    = False,
+                    nonecheck        = False,
+                    binding          = True,
+                    # embedsignature = True,
                     )
 
 from setuptools import find_packages, find_namespace_packages
@@ -77,9 +65,8 @@ pbase = "imi"
 package_data = {"" : "*.pxd *.pyx".split(),
                 # "imi.core" : "*.pxd *.pyx".split(),
                 }
-packages = find_packages()
-print(packages)
-setup(\
+packages = find_packages(exclude = ["Plexsim*"])
+setup(
         name         = 'imi',
         version      = __version__,
         author       = "Casper van Elteren",
@@ -87,13 +74,13 @@ setup(\
         url          = "cvanelteren.github.io",
         zip_safe     = False,
         # namespace_packages     = namespaces,
-        # package_dir = {"" : "imi"},
-        package_data = package_data,\
+        include_packaged_data = True,
+        # package_data = package_data,
         packages     = packages,
         ext_modules  = cythonize(
                 exts,
                 compiler_directives = cdirectives,
                 nthreads            = mp.cpu_count(),
-                ),\
+                ),
 
 )
