5e153d34eaf2f473efefd6ba8bde016a2da16fe6
cvanelteren <caspervanelteren@gmail.com>
2020-10-26 13:09:57 +0100

    added output check easy

--- slurm/monitor.sh
+++ slurm/monitor.sh
@@ -2,4 +2,4 @@ echo "Number of simulations running"
 squeue | grep cveltere | wc -l
 echo "Disk space in scratch" && du -sh /var/scratch/cveltere
 squeue | grep cveltere
-cat *"$(squeue -u cveltere -h | awk '$0=$1'"*
+cat *"$(squeue -u cveltere -h | awk '$0=$1')"*
